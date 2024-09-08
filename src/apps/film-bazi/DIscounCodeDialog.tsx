e4dab0ef9a31ca5acbdce5f130e8d6dd596d5747
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2024-09-09 00:43:18 +0330

    temp: disable backend api call temporarily

--- src/apps/film-bazi/DIscounCodeDialog.tsx
+++ src/apps/film-bazi/DIscounCodeDialog.tsx
@@ -6,12 +6,18 @@ import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import { toPersianNumber } from 'commons/utils/translateNumber';
+
+type DiscountCodeType = {
+  code: string;
+  usageCount: number;
+}
 
 type DiscountDialogProps = {
   open: boolean;
   onClose: () => void;
   filmName: string;
-  discountCode: string;
+  discountCode: DiscountCodeType;
 };
 
 const DiscountDialog: React.FC<DiscountDialogProps> = ({
@@ -28,8 +34,11 @@ const DiscountDialog: React.FC<DiscountDialogProps> = ({
         <DialogContentText>
           {`از کد تخفیف زیر برای دعوت دوستان خود به تماشای فیلم ${filmName} استفاده کنید.`}
         </DialogContentText>
-        <Typography variant="h4" component="div" sx={{ mt: 2, textAlign: 'center', fontWeight: 'bold' }}>
-          {discountCode}
+        <Typography variant="h3" component="div" sx={{ mt: 2, textAlign: 'center', fontWeight: 'bold' }}>
+          {discountCode.code}
+        </Typography>
+        <Typography variant="h5" component="div" sx={{ mt: 2, textAlign: 'center', fontWeight: 'bold' }}>
+          {`تا حالا 🤩${toPersianNumber(discountCode.usageCount)} نفر از کد تخفیف شما استفاده کرده‌اند.`}
         </Typography>
       </DialogContent>
       <DialogActions>
