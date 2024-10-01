0882df709b4eb39abdcd34b057ae72bc74e91b9b
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2024-10-01 12:01:03 +0330

    fix: fix some minor issues in ScoreBoard page

--- src/apps/film-bazi/pages/ScoreBoard.tsx
+++ src/apps/film-bazi/pages/ScoreBoard.tsx
@@ -1,6 +1,4 @@
 import React, { Fragment, useEffect, useState } from 'react';
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import useGetScoreBoard from '../hooks/useGetScoreBoard';
 import AppBarComponent from '../components/organisms/Appbar';
 import useGetMyRank from '../hooks/useGetMyRank';
@@ -8,91 +6,91 @@ import { useSelector } from 'react-redux';
 import CompetitionScores from '../components/organisms/CompetitionScores';
 import { Container } from '@mui/material';
 import backgroundImg from "../assets/background.png";
-import { Scoreboard } from '@mui/icons-material';
 import { ScoreBoardItemType } from '../types';
 import useGetMyBalances from '../hooks/useGetMyBalances';
 
-interface scoreRecordsStateProp{
-    winnerUsersInfo: ScoreBoardItemType[],
-    winnerScoresInfo: {rank: number, score: number}[],
-    currentUser: ScoreBoardItemType,
-    currentUserExistsInWinners: boolean
+interface ScoreRecordsStateProp {
+	winnerUsersInfo: ScoreBoardItemType[],
+	winnerScoresInfo: { rank: number, score: number }[],
+	currentUser: ScoreBoardItemType,
+	currentUserExistsInWinners: boolean
 }
 
 
 
 const App: React.FC = () => {
-	let { scoreBoard, loading: scoreBoardLoading } = useGetScoreBoard();
-	const {rank: myRank, loading: myRankLoading, error: myRankError} = useGetMyRank();
-	const {balances, loading: balancesLoading} = useGetMyBalances();
-    const userAccount = useSelector((state: any) => state.account);
-    const [winnerScores, setWinnerScores] = useState([]);
-    const [scoreRecordsState, setScoreRecordsState] = useState<scoreRecordsStateProp>({winnerUsersInfo: [],winnerScoresInfo: [], currentUser: null, currentUserExistsInWinners: false});
-    
+	const { scoreBoard, loading: scoreBoardLoading } = useGetScoreBoard();
+	const { rank: myRank, loading: myRankLoading, error: myRankError } = useGetMyRank();
+	const { balances, loading: balancesLoading } = useGetMyBalances();
+	const userAccount = useSelector((state: any) => state.account);
+	const [winnerScores, setWinnerScores] = useState([]);
+	const [scoreRecordsState, setScoreRecordsState] = useState<ScoreRecordsStateProp>({ winnerUsersInfo: [], winnerScoresInfo: [], currentUser: null, currentUserExistsInWinners: false });
+
 	useEffect(() => {
 		if (scoreBoard.length > 0) {
 			const ranks = [];
-			for(let i = 1; i < 4; i++){
+			for (let i = 1; i < 4; i++) {
 				let rank = scoreBoard.find(record => record.rank === i);
-				if(rank != null){
-					ranks.push({rank: i, score: rank.score});
+				if (rank != null) {
+					ranks.push({ rank: i, score: rank.score });
 				}
 			}
-            setWinnerScores(ranks);
+			setWinnerScores(ranks);
 		}
-	  }, [scoreBoardLoading]);
+	}, [scoreBoardLoading]);
 
 
 	useEffect(() => {
-		if(!scoreBoardLoading && !myRankLoading && !balancesLoading){
-            let newRecords = [...scoreBoard];
-            for(let i = 0; i < newRecords.length; i++){
-				Object.defineProperty(newRecords[i], "currentUser", {value: false, writable: true});
+		if (!scoreBoardLoading && !myRankLoading && !balancesLoading) {
+			let newRecords = [...scoreBoard];
+			for (let i = 0; i < newRecords.length; i++) {
+				Object.defineProperty(newRecords[i], "currentUser", { value: false, writable: true });
 			}
-            let exists = false;
-            if(myRank.rank != null){
-                let currentUserInRecords = (newRecords.find(record => (record.id === userAccount.userInfo.id )));
-                if (currentUserInRecords != null){
-                    currentUserInRecords.currentUser = true;
-                    exists = true;
-                }
-            }
-            setScoreRecordsState({...scoreRecordsState, 
-                winnerUsersInfo: newRecords, 
-                currentUser: {
-                    first_name: userAccount.userInfo.first_name,
-                    last_name: userAccount.userInfo.last_name,
-                    rank: myRank.rank,
-                    currentUser: true,
-                    id: userAccount.userInfo.id,
-                    score: balances["filmbazi-coin"]
-                },
-                currentUserExistsInWinners: exists
-            });
+			let exists = false;
+			if (myRank.rank != null) {
+				let currentUserInRecords = (newRecords.find(record => (record.id === userAccount.userInfo.id)));
+				if (currentUserInRecords != null) {
+					currentUserInRecords.currentUser = true;
+					exists = true;
+				}
+			}
+			setScoreRecordsState({
+				...scoreRecordsState,
+				winnerUsersInfo: newRecords,
+				currentUser: {
+					first_name: userAccount.userInfo.first_name,
+					last_name: userAccount.userInfo.last_name,
+					rank: myRank.rank,
+					currentUser: true,
+					id: userAccount.userInfo.id,
+					score: balances["filmbazi-coin"]
+				},
+				currentUserExistsInWinners: exists
+			});
 		}
 	}, [scoreBoardLoading, myRankLoading, balancesLoading])
 
 
 	return (
-		<Container
-            maxWidth = {false}
-            sx = {{
-                backgroundImage: `url(${backgroundImg})`,
-                backgroundSize: "cover",
-                backgroundPosition: "center",
-                backgroundRepeat: "no-repeat",
-				backgroundAttachment: "fixed",
-                minHeight: '100vh',
-                minWidth: "100vw",
-                width: "100vw",
-            }}
-        >
-			<AppBarComponent />	
-			<CompetitionScores allScores={scoreRecordsState} winnerScores={winnerScores}/>
-		</Container>
+		<Fragment>
+			<AppBarComponent />
+			<Container
+				maxWidth={false}
+				sx={{
+					backgroundImage: `url(${backgroundImg})`,
+					backgroundSize: "cover",
+					backgroundPosition: "center",
+					backgroundRepeat: "no-repeat",
+					backgroundAttachment: "fixed",
+					minHeight: '100vh',
+					minWidth: "100vw",
+					width: "100vw",
+				}}
+			>
+				<CompetitionScores allScores={scoreRecordsState} winnerScores={winnerScores} />
+			</Container>
+		</Fragment>
 	);
 };
 
 export default App;
-
-
