b2872a3f3b5afa48cd5dea9e3827ef4a85033f5a
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2024-10-28 18:35:54 +0330

    fix: set correct login background

--- src/apps/ashbaria/pages/Login/index.tsx
+++ src/apps/ashbaria/pages/Login/index.tsx
@@ -1,12 +1,9 @@
 import { Box } from "@mui/material";
-import React, { FC, useEffect, useState } from "react";
-import backgroundImg from "../../assets/profileBackground.svg";
-import { useGetVerificationCodeMutation } from "apps/website-display/redux/features/user/UserSlice";
+import React, { FC, useState } from "react";
+import backgroundImg from "../../assets/login-background.jpg";
 import EnterVerificationCode from "./EnterVerificationCode";
 import EnterPhoneNumber from "./EnterPhoneNumber";
 import { useSearchParams } from "react-router-dom";
-import isPhoneNumber from "commons/utils/validators/isPhoneNumber";
-import { toast } from "react-toastify";
 
 export enum LoginTabs {
   EnterPhoneNumber = 'EnterPhoneNumber',
