e1e95b5756c46bf7fb6fa7870f545fa386ab7459
sfahmadzadeh <sfahmadzadeh15@gmail.com>
2024-11-06 15:16:16 +0330

    update skeleton sizes

--- src/apps/ashbaria/components/atoms/SingleScoreRecordSkeleton.tsx
+++ src/apps/ashbaria/components/atoms/SingleScoreRecordSkeleton.tsx
@@ -4,8 +4,8 @@ import { Box, Skeleton } from "@mui/material";
 export default function SingleScoreRecordSkeleton(){
     return(
         <Box marginBottom="10px" sx={{display:"flex", flexDirection:"row"}}>
-            <Skeleton variant="rounded" width="50px" height="60px" />
-            <Skeleton variant="rounded" height="50px" sx={{width: {xs: "200px", md: "600px"}}}/>
+            <Skeleton variant="rounded" width="60px" height="60px" />
+            <Skeleton variant="rounded" height="60px" sx={{width: {xs: "420px", md: "780px"}}}/>
         </Box>
     );
 }
