c0051d4fa53ea423d290e86aa3bff57f34041e10
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2024-11-10 14:28:03 +0330

    fix: handle free hints

--- src/apps/ashbaria/components/organisms/hint/Hint.tsx
+++ src/apps/ashbaria/components/organisms/hint/Hint.tsx
@@ -2,6 +2,7 @@ import React, { FC } from "react";
 import ViewHint from "./ViewHint";
 import BuyHint from "./BuyHint";
 import { PublicGeneralHint } from "commons/types/models";
+import { ASHBARIA_COIN } from "apps/ashbaria/constants/game-info";
 
 type HintPropsType = {
   hint: PublicGeneralHint;
@@ -12,7 +13,12 @@ const Hint: FC<HintPropsType> = ({
   hint,
   onClose,
 }) => {
-  if (hint.has_spent_on_object) {
+  // todo: attributes should be calculated in the backend?
+  const buyAttribute = hint?.attributes?.find(attribute => attribute.type === 'Buy')
+  const costAttribute = buyAttribute?.attributes?.find(attribute => attribute.type === 'Cost')
+  const ashbariaCost = costAttribute?.['value']?.[ASHBARIA_COIN];
+
+  if (hint.has_spent_on_object || !ashbariaCost) {
     return <ViewHint hint={hint} onClose={onClose} />
   } else {
     return <BuyHint hint={hint} onClose={onClose} />
