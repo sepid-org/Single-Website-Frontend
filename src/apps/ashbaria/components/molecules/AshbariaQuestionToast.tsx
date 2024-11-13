13b5199cc091dab71f24f0b5a092689b491ec60b
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2024-11-13 19:08:38 +0330

    fix: fix toast text

--- src/apps/ashbaria/components/molecules/AshbariaQuestionToast.tsx
+++ src/apps/ashbaria/components/molecules/AshbariaQuestionToast.tsx
@@ -42,7 +42,7 @@ const AshbariaQuestionToast = ({ ...props }) => {
 
   useEffect(() => {
     if (questionAnswers?.length > 0 && !hasToastBeenShown.current) {
-      toast.info('دوباره برگشتی همینجا. این دفعه یه راه جدید رو برو...', {
+      toast.info('دادبستان عزیز! دادگاه ادامه داره. لطفاً ادله‌ی بیشتری بیار...', {
         autoClose: 5000,
       });
       hasToastBeenShown.current = true;
