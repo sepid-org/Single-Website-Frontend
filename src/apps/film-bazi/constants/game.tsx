019cc08ca7878c6e1c4462e10679410fc01e330e
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2024-11-13 17:08:32 +0330

    fix: fix some minor bugs

--- src/apps/film-bazi/constants/game.tsx
+++ src/apps/film-bazi/constants/game.tsx
@@ -1,2 +1,4 @@
 export const FILMBAZI_COIN = 'filmbazi-coin';
+export const SHAD_ORIGIN = 'SHAD';
+export const VALUE_OF_EACH_SCORE = 20000;
 export const FILMBAZI_EXTRA_CHANCE = 'filmbazi-extra-chance';
