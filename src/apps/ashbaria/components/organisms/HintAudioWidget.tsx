e3f55c30e618da72b4c4ee717223a361ae142337
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2024-11-16 16:53:53 +0330

    feat: init beautiful audio widget for hints

---
+++ src/apps/ashbaria/components/organisms/HintAudioWidget.tsx
@@ -0,0 +1,41 @@
+import React, { Fragment, useEffect, useRef } from 'react';
+import AudioEditWidget from '../../../../commons/components/organisms/Widget/contents/AudioWidget/edit';
+import { WidgetModes } from '../../../../commons/components/organisms/Widget';
+export { AudioEditWidget };
+
+// todo: Ahmadzade
+const HintAudioWidget = ({
+  link,
+  autoplay,
+  mode,
+  repeat = false,
+  volume = 100,
+}) => {
+  const audioRef = useRef(null);
+
+  // Automatically start playback after the component appears if `autoplay` is true
+  useEffect(() => {
+    if (audioRef.current) {
+      audioRef.current.volume = Math.min(Math.max(volume, 0), 100) / 100; // set volume between 0 and 1
+    }
+
+    if (autoplay && audioRef.current && mode === WidgetModes.View) {
+      audioRef.current.play().catch((e) => {
+        // console.error("Auto-play prevented or an error occurred:", e);
+      });
+    }
+  }, [autoplay, volume]);
+
+  return (
+    <audio
+      ref={audioRef}
+      controls
+      loop={repeat}
+      style={{ width: '100%' }}
+      src={link}
+      preload="auto"
+    />
+  );
+};
+
+export default HintAudioWidget;
--- src/apps/ashbaria/components/organisms/hint/ViewHint.tsx
+++ src/apps/ashbaria/components/organisms/hint/ViewHint.tsx
@@ -6,6 +6,10 @@ import { useGetResourceByIdQuery } from "commons/redux/apis/cms/resource/Resourc
 import WidgetsPaper from "commons/template/Paper";
 import FullScreenPaper from "commons/components/atoms/FullScreenPaper";
 import ArrowRightIcon from "../../atoms/icons/ArrowRight";
+import { FSMProvider } from "commons/hooks/useFSMContext";
+import { FSMStateProvider } from "commons/hooks/useFSMStateContext";
+import WIDGET_REGISTRY, { WidgetRegistryType } from "commons/components/organisms/Widget/useWidgetFactory/WidgetTypeMapper";
+import HintAudioWidget from "../HintAudioWidget";
 
 type HintPropsType = {
   hint: PublicResourceType;
@@ -17,6 +21,14 @@ const ViewHint: FC<HintPropsType> = ({
   onClose,
 }) => {
   const { data: detailedHint } = useGetResourceByIdQuery({ resourceId: hint?.id }, { skip: !Boolean(hint?.id) });
+
+  const CUSTOM_WIDGET_REGISTRY: WidgetRegistryType = {
+    ...WIDGET_REGISTRY,
+    Audio: {
+      ...WIDGET_REGISTRY['Audio'],
+      WidgetComponent: HintAudioWidget,
+    },
+  }
 
   return (
     <Stack padding={3} spacing={2} component={FullScreenPaper} maxWidth={'xs'} position={'relative'}>
@@ -35,7 +47,12 @@ const ViewHint: FC<HintPropsType> = ({
       </Stack>
       <Stack height={'100%'} alignItems={'center'} justifyContent={'center'}>
         <Stack width={'100%'} spacing={2}>
-          <WidgetsPaper mode="general" paperId={detailedHint?.content['paper_id']} />
+          <FSMStateProvider
+            fsmStateId={null}
+            widgetRegistry={CUSTOM_WIDGET_REGISTRY}
+          >
+            <WidgetsPaper mode="general" paperId={detailedHint?.content['paper_id']} />
+          </FSMStateProvider>
         </Stack>
       </Stack>
     </Stack>
