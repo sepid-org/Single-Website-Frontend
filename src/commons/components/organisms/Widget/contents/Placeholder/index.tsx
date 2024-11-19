0f18fabdb4a8bba26a483406c63451a7ff8c90b3
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2024-11-20 01:30:30 +0330

    refactor: replace skeleton with gray box (in placeholder)

--- src/commons/components/organisms/Widget/contents/Placeholder/index.tsx
+++ src/commons/components/organisms/Widget/contents/Placeholder/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import EditablePlaceholder from './edit';
-import { Box } from '@mui/material';
+import { Skeleton } from '@mui/material';
 export { EditablePlaceholder };
 
 const Placeholder = ({ }) => {
   return (
-    <Box width={'100%'} height={'100%'} sx={{ backgroundColor: 'gray' }} />
+    <Skeleton variant='rounded' width={'100%'} height={'100%'} />
   );
 };
 
