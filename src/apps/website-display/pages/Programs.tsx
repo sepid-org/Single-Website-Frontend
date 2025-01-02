3d9742e42fae342f4bf529b5d2b811977bbbee05
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2025-01-02 23:54:38 +0330

    refactor: remove past and current programs sections

--- src/apps/website-display/pages/Programs.tsx
+++ src/apps/website-display/pages/Programs.tsx
@@ -1,8 +1,7 @@
-import { Divider, Grid, Typography, Stack } from '@mui/material';
+import { Grid, Typography, Stack } from '@mui/material';
 import React from 'react';
 import ProgramCard from 'commons/components/organisms/cards/ProgramCard';
 import Layout from 'commons/template/Layout';
-import { ProgramType } from 'commons/types/models';
 import ProgramCardSkeleton from 'commons/components/organisms/cards/ProgramCardSkeleton';
 import Banner from 'commons/components/molecules/Banner';
 import { useGetProgramsQuery } from 'apps/website-display/redux/features/program/ProgramSlice';
@@ -12,38 +11,19 @@ import NoDataFound from 'commons/components/molecules/NoDataFound';
 const Programs = ({ }) => {
   const { data: pageMetadata } = useGetPageMetadataQuery({ pageAddress: window.location.pathname });
   const {
-    data: programsData,
+    data: programs,
     isLoading,
     isSuccess,
   } = useGetProgramsQuery({});
-  const programs = programsData?.programs.filter(program => program.is_visible) || [];
-  const visiblePrograms = programs.filter(program => program.is_visible)
+  const visiblePrograms = programs?.programs.filter(program => program.is_visible) || [];
 
-  const activePrograms: ProgramType[] = visiblePrograms.filter((program: ProgramType) => program.is_active);
-  const inactivePrograms: ProgramType[] = visiblePrograms.filter((program: ProgramType) => !program.is_active);
-
-  const activeProgramsElement = (
+  const programsElement = (
     <Grid item container spacing={2} xs={12}>
-      {(isSuccess && activePrograms.length === 0) ?
+      {(isSuccess && visiblePrograms.length === 0) ?
         <Grid container justifyContent={'center'}>
           <NoDataFound />
         </Grid> :
-        activePrograms.map((program, index) => (
-          <Grid key={index} container item xs={12} sm={6} md={4} justifyContent='center' alignItems='flex-start' >
-            <ProgramCard program={program} />
-          </Grid>
-        ))
-      }
-    </Grid>
-  );
-
-  const inactiveProgramsElement = (
-    <Grid item container spacing={2} xs={12}>
-      {(isSuccess && inactivePrograms.length === 0) ?
-        <Grid container item justifyContent='center' alignItems='center'>
-          <NoDataFound />
-        </Grid> :
-        inactivePrograms.map((program, index) => (
+        visiblePrograms.map((program, index) => (
           <Grid key={index} container item xs={12} sm={6} md={4} justifyContent='center' alignItems='flex-start' >
             <ProgramCard program={program} />
           </Grid>
@@ -54,7 +34,7 @@ const Programs = ({ }) => {
 
   const skeletonElements = (
     <Grid item container spacing={2} xs={12}>
-      {[...Array(6)].map((_, index) => (
+      {[...Array(3)].map((_, index) => (
         <Grid key={index} container item xs={12} sm={6} md={4} justifyContent='center' alignItems='flex-start' >
           <ProgramCardSkeleton />
         </Grid>
@@ -69,19 +49,8 @@ const Programs = ({ }) => {
         <Typography variant="h1" align='center'>
           {'دوره‌‌ها'}
         </Typography>
-        <Typography variant='h2' gutterBottom>
-          {'دوره‌‌های در جریان'}
-        </Typography>
-        <Divider />
         <Grid container>
-          {isLoading ? skeletonElements : activeProgramsElement}
-        </Grid>
-        <Typography variant='h2' gutterBottom>
-          {'دوره‌‌های گذشته'}
-        </Typography>
-        <Divider />
-        <Grid container>
-          {isLoading ? skeletonElements : inactiveProgramsElement}
+          {isLoading ? skeletonElements : programsElement}
         </Grid>
       </Stack>
     </Layout>
