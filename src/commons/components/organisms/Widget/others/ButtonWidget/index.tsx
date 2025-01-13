f4f95a5185fc929e06160476a88d135a9868ff0f
sfahmadzadeh <sfahmadzadeh15@gmail.com>
2025-01-13 13:09:21 +0330

    fix: split extrcat path function

--- src/commons/components/organisms/Widget/others/ButtonWidget/index.tsx
+++ src/commons/components/organisms/Widget/others/ButtonWidget/index.tsx
@@ -6,34 +6,7 @@ import { WidgetModes } from '../..';
 import ButtonWidgetEditor from './edit';
 import useChangeState from 'commons/hooks/fsm/useChangeState';
 import useSubmitButton from 'commons/hooks/useSubmitButton';
-
-const extractSvgPath = (svgUrl: string) => {
-  return new Promise<string>((resolve, reject) => {
-    fetch(svgUrl)
-      .then((response) => response.text())
-      .then((svgText) => {
-        const parser = new DOMParser();
-        const doc = parser.parseFromString(svgText, 'image/svg+xml');
-        const paths = doc.querySelectorAll('path');
-        const dAttributes: string[] = [];
-
-        paths.forEach(path => {
-          const d = path.getAttribute('d');
-          if (d) {
-            dAttributes.push(d);
-          }
-        });
-
-        if (dAttributes.length > 0) {
-          const mergedD = dAttributes.join(' ');
-          resolve(mergedD);
-        } else {
-          reject('No path found in SVG');
-        }
-      })
-      .catch(reject);
-  });
-};
+import extractSvgPath from 'commons/utils/extractSVGPath';
 
 type ButtonWidgetPropsType = {
   label: string;
