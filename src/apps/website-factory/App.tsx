3fa327fdd374c4588fcc2860750651db44c6996c
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2025-01-18 19:36:41 +0330

    Feat: init landing-builder

--- src/apps/website-factory/App.tsx
+++ src/apps/website-factory/App.tsx
@@ -4,6 +4,7 @@ import { Route, Routes } from 'react-router-dom';
 import PrivateRoute from 'commons/routes/PrivateRoute';
 import WebsiteManagement from 'apps/website-factory/pages/WebsiteManagement';
 import NotFoundPage from 'commons/pages/NotFoundPage';
+import PageBuilder from './pages/PageBuilder';
 
 const App = () => {
 
@@ -11,6 +12,7 @@ const App = () => {
     <Routes>
       <Route path="/" element={<PrivateRoute loginUrl='/login/' />}>
         <Route path="" element={<WebsiteManagement />} />
+        <Route path="build-page" element={<PageBuilder />} />
       </Route>
 
       <Route path="*" element={<NotFoundPage />} />
---
+++ src/apps/website-factory/pages/PageBuilder.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+
+type PropsType = {}
+
+const PageBuilder: FC<PropsType> = ({ }) => {
+
+  return (
+    <h1>salam!</h1>
+  );
+};
+
+export default PageBuilder;
