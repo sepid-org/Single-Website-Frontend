a84486e12d026f52b9f8769a2381bd9351a4fc14
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2025-02-25 17:31:25 +0330

    Fix: don't show steps if user is registered in program

--- src/apps/program/pages/Registration.tsx
+++ src/apps/program/pages/Registration.tsx
@@ -1,4 +1,4 @@
-import { Stack, Grid } from '@mui/material';
+import { Stack, Grid, Backdrop, CircularProgress } from '@mui/material';
 import React, { FC, useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 
@@ -14,22 +14,12 @@ type PropsType = {}
 const Registration: FC<PropsType> = () => {
   const navigate = useNavigate();
   const { programSlug } = useParams();
-
-  const {
-    data: program,
-    isLoading: isGetProgramLoading,
-  } = useGetProgramQuery({ programSlug });
-
+  const { data: program } = useGetProgramQuery({ programSlug });
   const { isAuthenticated } = useUserAuthentication();
-
-  const {
-    data: registrationReceipt,
-    isLoading: isRegistrationReceiptLoading
-  } = useGetMyReceiptQuery(
+  const { data: registrationReceipt } = useGetMyReceiptQuery(
     { formId: program?.registration_form },
     { skip: !program?.registration_form || !isAuthenticated }
   );
-
   const {
     currentStepIndex,
     lastActiveStepIndex,
@@ -37,40 +27,45 @@ const Registration: FC<PropsType> = () => {
   } = useRegistrationSteps();
 
   useEffect(() => {
-    if (registrationReceipt?.is_participating) {
+    if (registrationReceipt?.is_participating === true) {
       navigate(`/program/${programSlug}/`);
     }
   }, [registrationReceipt, programSlug, navigate]);
 
-  if (isGetProgramLoading || isRegistrationReceiptLoading) {
-    return;
+  if (registrationReceipt?.is_participating === false) {
+    return (
+      <Layout appbarMode='GENERAL'>
+        <Grid
+          container
+          spacing={2}
+          alignItems={{ xs: 'center', md: 'start' }}
+          justifyContent={{ xs: 'center', md: 'flex-start' }}
+        >
+          <Grid
+            item
+            xs={12}
+            md={3}
+            position={{ xs: 'static', md: 'sticky' }}
+            top={0}
+          >
+            <MyStepper steps={steps} activeStepIndex={lastActiveStepIndex} />
+          </Grid>
+          <Grid item xs={12} md={9}>
+            <Stack>
+              {steps[currentStepIndex]?.component}
+            </Stack>
+          </Grid>
+        </Grid>
+      </Layout>
+    );
   }
 
   return (
-    <Layout appbarMode='GENERAL'>
-      <Grid
-        container
-        spacing={2}
-        alignItems={{ xs: 'center', md: 'start' }}
-        justifyContent={{ xs: 'center', md: 'flex-start' }}
-      >
-        <Grid
-          item
-          xs={12}
-          md={3}
-          position={{ xs: 'static', md: 'sticky' }}
-          top={0}
-        >
-          <MyStepper steps={steps} activeStepIndex={lastActiveStepIndex} />
-        </Grid>
-        <Grid item xs={12} md={9}>
-          <Stack>
-            {steps[currentStepIndex]?.component}
-          </Stack>
-        </Grid>
-      </Grid>
-    </Layout>
-  );
+    <Backdrop open={true}>
+      <CircularProgress color="inherit" />
+    </Backdrop>
+  )
+
 };
 
 export default Registration;
