e9b8ede9babc1db191ec9aea5af2b1bf1976ef11
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2025-03-02 00:06:41 +0330

    Fix: fix setting appbar from website data

--- src/commons/components/organisms/Appbar/modes/DashboardAppbarItems.tsx
+++ src/commons/components/organisms/Appbar/modes/DashboardAppbarItems.tsx
@@ -3,37 +3,19 @@ import React from 'react';
 import DashboardButton from '../components/DashboardButton';
 import { useGetPageMetadataQuery, useGetWebsiteQuery } from 'apps/website-display/redux/features/WebsiteSlice';
 import WebsiteLogo from 'commons/components/atoms/logos/WebsiteLogo';
-import DefaultAppbarItems from './DefaultAppbarItems';
 import UserInfo from '../components/UserInfo';
-import { useSelector } from 'react-redux';
 import NotificationButton from 'apps/chat/components/atoms/NotificationButton';
 import useUserAuthentication from 'commons/hooks/useUserAuthentication';
 
 const DashboardAppbarItems = ({ }) => {
   const { data: pageMetadata } = useGetPageMetadataQuery({ pageAddress: window.location.pathname });
-  const { data: websitedata } = useGetWebsiteQuery();
+  const { data: websiteData } = useGetWebsiteQuery();
   const { isAuthenticated } = useUserAuthentication();
-
-  if (!pageMetadata?.appbar?.body && websitedata?.appbar?.body) {
-    return DefaultAppbarItems({})
-  }
 
   const desktopLeftItems = [];
   const desktopRightItems = [];
 
-  if (!pageMetadata.appbar.body) {
-    websitedata.appbar.body.desktopLeftItems.filter(item => item.position === 'left').forEach((item, index) => {
-      desktopLeftItems.push(
-        <DashboardButton key={index} label={item.label} to={item.to} items={item.items} />
-      );
-    });
-    websitedata.appbar.body.desktopLeftItems.filter(item => item.position === 'right').forEach((item, index) => {
-      desktopRightItems.push(
-        <DashboardButton key={index} label={item.label} to={item.to} items={item.items} />
-      );
-    });
-  }
-  else{
+  if (pageMetadata.appbar?.body) {
     pageMetadata.appbar.body.desktopLeftItems.filter(item => item.position === 'left').forEach((item, index) => {
       desktopLeftItems.push(
         <DashboardButton key={index} label={item.label} to={item.to} items={item.items} />
@@ -44,8 +26,19 @@ const DashboardAppbarItems = ({ }) => {
         <DashboardButton key={index} label={item.label} to={item.to} items={item.items} />
       );
     });
+  } else {
+    websiteData.appbar.body.desktopLeftItems.filter(item => item.position === 'left').forEach((item, index) => {
+      desktopLeftItems.push(
+        <DashboardButton key={index} label={item.label} to={item.to} items={item.items} />
+      );
+    });
+    websiteData.appbar.body.desktopLeftItems.filter(item => item.position === 'right').forEach((item, index) => {
+      desktopRightItems.push(
+        <DashboardButton key={index} label={item.label} to={item.to} items={item.items} />
+      );
+    });
   }
-  
+
   const websiteLogo = <WebsiteLogo />;
   const userInfo = <UserInfo />
   const notificationButton = <NotificationButton />
