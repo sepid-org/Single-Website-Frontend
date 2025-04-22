838da78019a5c2b6b3a0b2bdf1e24938b8b37e69
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2025-04-22 15:50:25 +0330

    Temp: remove game button from filmbazi dashboard

--- src/apps/film-bazi/components/organisms/DashboardSidebar.tsx
+++ src/apps/film-bazi/components/organisms/DashboardSidebar.tsx
@@ -39,10 +39,10 @@ const DashboardSidebar: FC<DashboardSidebarPropsType> = ({
       <ProgramContactInfo programContactInfo={program.program_contact_info} />
       <Stack spacing={2} justifyContent={'space-between'}>
         <MyInfoChip />
-        {tab === 'films' ?
+        {/* {tab === 'films' ?
           <DashboardButton2 label='بازی‌ها' icon={<CupIcon />} onClick={() => { localNavigate(`/games/`) }} /> :
           <DashboardButton2 label='فیلم‌ها' icon={<MovieIcon />} onClick={() => { localNavigate(`/films/`) }} />
-        }
+        } */}
         <DashboardButton label='سرمایه من' icon={<TicketIcon />} onClick={() => { localNavigate(`/capital/`) }} />
         <DashboardButton label='جدول امتیازات' icon={<RankingIcon />} onClick={() => { localNavigate(`/scoreboard/`) }} />
         {programPermissions?.is_manager &&
