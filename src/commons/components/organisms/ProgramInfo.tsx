9fc95b403915610c1109f55973b8d9f574ed424d
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2025-06-13 02:41:46 +0330

    Refactor: refactor registration form header

--- src/commons/components/organisms/ProgramInfo.tsx
+++ src/commons/components/organisms/ProgramInfo.tsx
@@ -1,16 +1,13 @@
-import { Stack, Paper, Typography } from '@mui/material';
+import { Stack, Paper, Typography, Box } from '@mui/material';
 import React, { FC } from 'react';
 import { ProgramType } from 'commons/types/models';
 import useWidth from 'commons/utils/UseWidth';
-import { toPersianNumber } from 'commons/utils/translateNumber';
 
 type ProgramInfoPropsType = {
   program: ProgramType;
 }
 
-const ProgramInfo: FC<ProgramInfoPropsType> = ({
-  program,
-}) => {
+const ProgramInfo: FC<ProgramInfoPropsType> = ({ program }) => {
   const width = useWidth();
 
   return (
@@ -19,47 +16,45 @@ const ProgramInfo: FC<ProgramInfoPropsType> = ({
       direction={{ xs: 'column', sm: 'row' }}
       sx={{
         width: '100%',
-        padding: '0px !important',
+        height: '100%',
+        p: 0,
         display: 'flex',
       }}
-      alignItems='center'
-      justifyContent="space-between">
-      <img
-        src={program.cover_image}
-        alt=""
-        style={width == 'xs' ? {
-          flex: 0,
-          borderTopLeftRadius: 5,
-          borderTopRightRadius: 5,
-          width: '100%',
-          objectFit: 'cover',
-        } : {
-          flex: 0,
-          borderTopRightRadius: 5,
-          borderBottomRightRadius: 5,
-          maxWidth: '40%',
-          objectFit: 'cover',
+      alignItems="stretch"
+      justifyContent="space-between"
+    >
+      <Box
+        sx={{
+          flex: width === 'xs' ? '0 0 auto' : '0 0 40%',
+          width: width === 'xs' ? '100%' : undefined,
+          aspectRatio: '1 / 1',
+          overflow: 'hidden',
         }}
-      />
-      <Stack spacing={2} sx={{ padding: 2, flex: 1 }}>
-        {program.name && (
-          <Typography gutterBottom align="center" variant="h1">{program.name}</Typography>
-        )}
-        <Typography align="center">{program.description}</Typography>
-        {/* todo: this sentence may cause misunderstanding: */}
-        {/* {program.participation_type == 'Team' && (
-          <Typography align="center">{`شرکت در این دوره در قالب تیم‌های ${toPersianNumber(program.team_size)} نفره امکان‌پذیر است.`}</Typography>
-        )} */}
-        {program.participation_type == 'Individual' && (
-          <Typography align="center">{'شرکت در این دوره به صورت فردی است.'}</Typography>
-        )}
-        {program.is_free &&
-          <Typography align="center">{'هزینه‌ی ثبت‌نام رایگان است!'}</Typography>
-        }
-        <div />
+      >
+        <img
+          src={program.cover_image}
+          alt={program.name}
+          style={{
+            width: '100%',
+            height: '100%',
+            objectFit: 'cover',
+            display: 'block',
+            borderTopLeftRadius: width === 'xs' ? 5 : 0,
+            borderTopRightRadius: 5,
+            borderBottomRightRadius: width === 'xs' ? 0 : 5,
+          }}
+        />
+      </Box>
+
+      <Stack spacing={2} sx={{ p: 2, flex: 1 }}>
+        <Typography gutterBottom variant="h1">
+          {program.name}
+        </Typography>
+        <Typography variant="body1">
+          {program.description}
+        </Typography>
       </Stack>
     </Stack>
-
   );
 };
 
